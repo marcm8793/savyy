1f0bf5e2fbe3e7bc412669f8a2fb769938b0583d
Marc Mansour <marcm9387@gmail.com>
2025-06-06 22:30:04 +0200

    Refactor folders structure

--- backend/config/tink.config.ts
+++
@@ -1,6 +0,0 @@
-// Tink API configuration
-export const tinkConfig = {
-  clientId: process.env.TINK_CLIENT_ID || "",
-  clientSecret: process.env.TINK_CLIENT_SECRET || "",
-  apiUrl: process.env.TINK_API_URL,
-};
---
+++ server/src/utils/tink.ts
@@ -0,0 +1,5 @@
+// Tink API configuration
+export const tinkConfig = {
+  clientId: process.env.TINK_CLIENT_ID,
+  clientSecret: process.env.TINK_CLIENT_SECRET,
+};

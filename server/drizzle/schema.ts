fb9fc9d23d10935519ab36a41aeb211b039dd0ed
Marc Mansour <marcm9387@gmail.com>
2025-06-06 23:15:52 +0200

    client setup

--- server/drizzle/schema.ts
+++ server/drizzle/schema.ts
@@ -1,6 +1,6 @@
-import { user } from "../src/models/user.model";
-import { transaction } from "../src/models/transaction.model";
-import { account } from "../src/models/account.model";
+import { user } from "./models/user.model";
+import { transaction } from "./models/transaction.model";
+import { account } from "./models/account.model";
 
 // Export all schemas for Drizzle migrations
 export const schema = {
